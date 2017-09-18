bf1f264db8a89caaf900ecd0cbc911ab5a4a672b
Shao GaoJie <ShaoGaoJie@ShaodeMacBook-Pro.local>
2017-09-18 19:02:02 +0800

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+//var_dump(env('LARAVELBLOG_KEY'));
+//print_r($_SERVER);
+Route::get('/', function () {
+    return view('welcome');
+});
