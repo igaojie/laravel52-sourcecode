4d8383ca4ed658283f5c9a12764a3128d1cad880
邵高杰 <shaogaojie@xin.com>
2018-05-16 17:39:05 +0800

    TEST PHPCS

---
+++ app/Http/Controllers/a.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * a.php
+ * PHP Version
+ * Created by PhpStorm.
+ * @author: ShaoGaoJie <shaogaojie@xin.com>
+ * @time: 2018/5/16 下午5:17
+ * @copyright 2018 优信拍（北京）信息科技有限公司
+ * @license http://www.xin.com
+ * @link shaogaojie@xin.com
+ */
+
+function a()
+{
+}
+
+class a
+{
+    public function __construct()
+    {
+        aa();
+        CC();
+    }
+}
